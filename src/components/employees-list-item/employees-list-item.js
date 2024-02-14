edc33c33d5cf4de4f8484df04b7ba438aec385f3
Timur <Stafr95@mail.ru>
2024-02-14 15:01:35 +0500

    Working with state

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -5,9 +5,19 @@ import './employees-list-item.scss'
 class EmployeesListItem extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			increase: false
+		}
+	}
+
+	onIncrease = () => {
+		this.setState(({increase}) => ({
+			increase: !increase
+		}))
 	}
 	render() {
-		const {name, salary, increase} = this.props;
+		const {name, salary} = this.props;
+		const {increase} = this.state;
 
 		let classNames = "list-group-item d-flex justify-content-between"
 
@@ -20,13 +30,18 @@ class EmployeesListItem extends Component {
 				<span className="list-group-item-label">{name}</span>
 				<input type="text" className="list-group-item-input" defaultValue={salary + ' $'}/>
 				<div className='d-flex justify-content-center align-items-center'>
-					<button type="button"
-							className="btn-cookie btn-sm ">
+					<button
+						type="button"
+						className="btn-cookie btn-sm "
+						onClick={this.onIncrease}
+					>
 						<i className="fas fa-cookie"></i>
 					</button>
 
-					<button type="button"
-							className="btn-trash btn-sm ">
+					<button
+						type="button"
+						className="btn-trash btn-sm "
+					>
 						<i className="fas fa-trash"></i>
 					</button>
 					<i className="fas fa-star"></i>
