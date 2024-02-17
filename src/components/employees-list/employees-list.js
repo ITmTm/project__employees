72ed4a843cca28eabb3ac1780ec328ced650b56e
Timur <Stafr95@mail.ru>
2024-02-17 16:03:27 +0500

    combining two methods into one

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,7 +2,7 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import './employees-list.scss'
 
-const EmployeesList = ({data, onDelete, onToggleIncrease, onToggleLike}) => {
+const EmployeesList = ({data, onDelete, onToggleProp}) => {
 
 	const elements = data.map(item => {
 		const {id, ...itemProps} = item;
@@ -11,8 +11,7 @@ const EmployeesList = ({data, onDelete, onToggleIncrease, onToggleLike}) => {
 				key={id}
 				{...itemProps}
 				onDelete={() => onDelete(id)}
-				onToggleIncrease={() => onToggleIncrease(id)}
-				onToggleLike={() => onToggleLike(id)}
+				onToggleProp={(e) => onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}
 			/>
 		)
 	})
