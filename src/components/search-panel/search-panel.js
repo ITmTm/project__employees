8b353f6e136b619e3bc76fa408cca4a6638fc2fe
Timur <Stafr95@mail.ru>
2024-02-21 17:02:57 +0500

    Using raising local state to parent

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -7,12 +7,20 @@ class SearchPanel extends Component {
 			term: ''
 		}
 	}
+
+	onUpdateSearch = (e) => {
+		const term = e.target.value;
+		this.setState({term});
+		this.props.onUpdateSearch(term)
+	}
 	render() {
 		return (
 			<input
 				type="text"
 				className='form-control search-input'
 				placeholder='Найти сотрудника'
+				value={this.state.term}
+				onChange={this.onUpdateSearch}
 			/>
 		)
 	}
